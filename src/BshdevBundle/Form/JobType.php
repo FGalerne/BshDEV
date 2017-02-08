1ea4a16117907169e722cb19edf9d6dbb449fbf2
Florian GALERNE <florian.galerne@free.fr>
2017-02-08 15:08:18 +0100

    create crud for the jobs offers

---
+++ src/BshdevBundle/Form/JobType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BshdevBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class JobType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('description')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BshdevBundle\Entity\Job'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'bshdevbundle_job';
+    }
+
+
+}
