3cfbff4337922ee6b0d31359ce8de6416bffe03d
Florian GALERNE <florian.galerne@free.fr>
2017-02-10 09:26:58 +0100

    create views (history, contact and references)

--- src/BshdevBundle/Controller/DefaultController.php
+++ src/BshdevBundle/Controller/DefaultController.php
@@ -6,13 +6,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
-    {
-        return $this->render('BshdevBundle:Default:index.html.twig');
-    }
+
     public function homeAction()
     {
         return $this->render('BshdevBundle::homepage.html.twig');
+    }
+
+    public function contactAction()
+    {
+        return $this->render('BshdevBundle::contact.html.twig');
+    }
+
+    public function referencesAction()
+    {
+        return $this->render('BshdevBundle::references.html.twig');
+    }
+
+    public function historyAction()
+    {
+        return $this->render('BshdevBundle::history.html.twig');
     }
 
 }
