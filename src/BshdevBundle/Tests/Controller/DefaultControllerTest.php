ba6f49eb518912cc4332127287e22a5d185ac453
Florian GALERNE <florian.galerne@free.fr>
2017-02-23 11:15:52 +0100

    test ok for the navbar (no connect)

--- src/BshdevBundle/Tests/Controller/DefaultControllerTest.php
+++ src/BshdevBundle/Tests/Controller/DefaultControllerTest.php
@@ -18,6 +18,8 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals('BshdevBundle\Controller\DefaultController::homeAction', $client->getRequest()->attributes->get('_controller'));
         $this->assertTrue(200 === $client->getResponse()->getStatusCode());
 
+
+        //this links are on the navbar
         //Connexion button test
         $link = $crawler
             ->filter('a:contains("Connexion")')
@@ -29,8 +31,76 @@ class DefaultControllerTest extends WebTestCase
         $this->assertEquals('FOS\UserBundle\Controller\SecurityController::loginAction', $client->getRequest()->attributes->get('_controller'));
         $this->assertTrue(200 === $client->getResponse()->getStatusCode());
 
+        //Accueil button test
+        $link = $crawler
+            ->filter('a:contains("Accueil")')
+            ->eq(0)
+            ->link();
+
+        $crawler = $client->click($link);
+
+        $this->assertEquals('BshdevBundle\Controller\DefaultController::homeAction', $client->getRequest()->attributes->get('_controller'));
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+
+        //A propos button test
+        $link = $crawler
+            ->filter('a:contains("A propos")')
+            ->eq(0)
+            ->link();
+
+        $crawler = $client->click($link);
+
+        $this->assertEquals('BshdevBundle\Controller\DefaultController::historyAction', $client->getRequest()->attributes->get('_controller'));
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+
+        //Références button test
+        $link = $crawler
+            ->filter('a:contains("Références")')
+            ->eq(0)
+            ->link();
+
+        $crawler = $client->click($link);
+
+        $this->assertEquals('BshdevBundle\Controller\DefaultController::referencesAction', $client->getRequest()->attributes->get('_controller'));
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+
+        //Nous contacter button test
+        $link = $crawler
+            ->filter('a:contains("Nous contacter")')
+            ->eq(0)
+            ->link();
+
+        $crawler = $client->click($link);
+
+        $this->assertEquals('BshdevBundle\Controller\DefaultController::contactAction', $client->getRequest()->attributes->get('_controller'));
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+
+        //Nos Offres d'emploi button test
+        $link = $crawler
+            ->filter('a:contains("Nos Offres d\'emploi")')
+            ->eq(0)
+            ->link();
+
+        $crawler = $client->click($link);
+
+        $this->assertEquals('BshdevBundle\Controller\JobController::indexAction', $client->getRequest()->attributes->get('_controller'));
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+
+        //Candidature spontanée button test
+        $link = $crawler
+            ->filter('a:contains("Candidature spontanée")')
+            ->eq(0)
+            ->link();
+
+        $crawler = $client->click($link);
+
+        $this->assertEquals('BshdevBundle\Controller\PersonController::spontaCandidatureAction', $client->getRequest()->attributes->get('_controller'));
+        $this->assertTrue(200 === $client->getResponse()->getStatusCode());
+
 
     }
+
+
 }
 
 
